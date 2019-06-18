8c9f27c71be23d761f14fe7d3553a0046d0628c7
Az <aztecian1122@gmail.com>
2019-06-18 15:30:15 +0100

    added score. need re work switch statment

--- main.js
+++ main.js
@@ -1,54 +1,73 @@
-// Selecting user choice.
-// prompt user to input choice.
-// make sure choice is validated (cant chose anything but RPS).
-let userChoice = prompt('Your choices... Rock, Paper, Scissors');
-
-if (userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {
-    console.log(`you chose ${userChoice}!`);
-} else {
-    console.log('Invalid Choice!');
-}
+const userScore = 0;
+const computerScore = 0;
 
 // Select Computer choice.
 // store choice in an array.
 // make a random number generate. Add to choice via array.
 function getComputerChoice() {
-    const choice = ['rock', 'paper', 'scissors']
-    const randomNum = Math.floor(Math.random() * 3);
+  const choice = ["rock", "paper", "scissors"];
+  const randomNum = Math.floor(Math.random() * 3);
 
-    // return random choice every time (rps)
-    return `computer chose ${choice[randomNum]}`;
+  // return random choice every time (rps)
+  return choice[randomNum];
 }
-console.log(getComputerChoice());
 
-
-//Game Function.
+//Round Function.
 // Decide who wins by using switch statment.
+function playRound(userChoice, computerChoice) {
+  switch (userChoice + computerChoice) {
+    case ("rock", "scissors"):
+    case ("paper", "rock"):
+    case ("scissors", "paper"):
+      console.log(`You Win! ${userChoice} beats ${computerChoice}`);
+      win(userChoice, computerChoice);
+      break;
 
-function game(userChoice) {
-    const computerChoice = getComputerChoice;
+    case ("scissors", "rock"):
+    case ("rock", "paper"):
+    case ("paper", "scissors"):
+      console.log(`You Lost! ${userChoice} dont beat ${computerChoice}`);
+      lose(userChoice, computerChoice);
+      break;
 
-    switch (userChoice + computerChoice) {
-        case 'rock', 'scissors':
-        case 'paper', 'rock':
-        case 'scissors', 'paper':
-            win(userChoice, computerChoice);
-            break;
+    case ("rock", "rock"):
+    case ("paper", "paper"):
+    case ("scissors", "scissors"):
+      console.log(`You both Tie! ${userChoice}  ${computerChoice}`);
+      draw(userChoice, computerChoice);
+      break;
+  }
+}
 
-        case 'paper', 'scissors':
-        case 'scissors', 'rock':
-        case 'rock', 'paper':
-            lose(userChoice, computerChoice);
-            break;
+function score(winner) {
+  if (winner === win) {
+    userScore++;
+    return console.log(
+      `The score is Player: ${userScore} vs Computer ${computerScore}`
+    );
+  } else if (winner === lose) {
+    computerScore++;
+    return console.log(
+      `The score is Player: ${userScore} vs Computer ${computerScore}`
+    );
+  } else {
+    return console.log(
+      `The score is Player: ${userScore} vs Computer ${computerScore}`
+    );
+  }
+}
+// Selecting user choice.
+// prompt user to input choice.
+// make sure choice is validated (cant chose anything but RPS).
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const userChoice = prompt("Your Choices... Rock, Paper, Scissors");
+    const computerChoice = getComputerChoice();
 
-        case 'paper', 'paper':
-        case 'scissors', 'scissors':
-        case 'rock', 'rock':
-            draw(userChoice, computerChoice);
-            break;
-
-    }
-
+    console.log(`You chose ${userChoice}`);
+    console.log(`Computer chose ${computerChoice}`);
+    console.log(score(playRound(userChoice, computerChoice)));
+  }
 }
 
 game();
