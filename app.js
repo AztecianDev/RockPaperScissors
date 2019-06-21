2bdb2546b47b4eb6785a429bd3f03e678d674542
Az <aztecian1122@gmail.com>
2019-06-21 13:08:52 +0100

    add play function, get winner function and get computer choice function

--- app.js
+++ app.js
@@ -1,85 +1,51 @@
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const result_p = document.querySelector("result > p");
+const choices = document.querySelectorAll('.choice');
 let userScore = 0;
 let computerScore = 0;
 
-// Select Computer choice.
-// store choice in an array.
-// make a random number generate. Add to choice via array.
+// Play game
+function play(e) {
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+
+    console.log(playerChoice, computerChoice, winner);
+}
+
+//  Get computer choice
 function getComputerChoice() {
     const choice = ["rock", "paper", "scissors"];
     const randomNum = Math.floor(Math.random() * 3);
 
     // return random choice every time (rps)
     return choice[randomNum];
+
 }
 
-//Round Function.
-// Decide who wins by using switch statment.
-function playRound(userChoice, computerChoice) {
-    if (userChoice === computerChoice) {
-        console.log(
-            `It\'s a Tie! You chose ${userChoice} and Computer chose ${computerChoice} `
-        );
+function getWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return 'draw';
+        // console.log(
+        //     `It\'s a Tie! You chose ${playerChoice} and Computer chose ${computerChoice} `
+        // );
     } else if (
-        (userChoice === "rock" && computerChoice === "scissors") ||
-        (userChoice === "paper" && computerChoice === "rock") ||
-        (userChoice === "scissors" && computerChoice === "paper")
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
     ) {
-        console.log(`You Win! ${userChoice} beats ${computerChoice}`);
+        // console.log(`You Win! ${playerChoice} beats ${computerChoice}`);
         return "win";
     } else if (
-        (userChoice === "scissors" && computerChoice === "rock") ||
-        (userChoice === "rock" && computerChoice === "paper") ||
-        (userChoice === "paper" && computerChoice === "scissors")
+        (playerChoice === "scissors" && computerChoice === "rock") ||
+        (playerChoice === "rock" && computerChoice === "paper") ||
+        (playerChoice === "paper" && computerChoice === "scissors")
     ) {
-        console.log(`You Lose! ${userChoice} lost to ${computerChoice}`);
+        // console.log(`You Lose! ${playerChoice} lost to ${computerChoice}`);
         return "lose";
     }
 }
-//score for player and computer.
-function score(winner) {
-    if (winner === "win") {
-        userScore++;
-        console.log(
-            `The score is Player: ${userScore} vs Computer ${computerScore}`
-        );
-    } else if (winner === "lose") {
-        computerScore++;
-        console.log(
-            `The score is Player: ${userScore} vs Computer ${computerScore}`
-        );
-    } else {
-        console.log(
-            `The score is Player: ${userScore} vs Computer ${computerScore}`
-        );
-    }
-}
-// Selecting user choice.
-// prompt user to input choice.
-// make sure choice is validated (cant chose anything but RPS).
-// loop 5 times.
-function game() {
-    //   for (let i = 0; i < 5; i++) {
-    //     const userChoice = prompt("Your Choices... Rock, Paper, Scissors");
-    //     const computerChoice = getComputerChoice();
 
-    //     console.log(`You chose ${userChoice}`);
-    //     console.log(`Computer chose ${computerChoice}`);
-    //     console.log(score(playRound(userChoice, computerChoice)));
-    //   }
-    // print final score outcome to user at end of rounds(loop).
-    if (userScore > computerScore) {
-        console.log(
-            `Well done you WON! Player Score:${userScore} - Computer Score: ${computerScore}`
-        );
-    } else if (userScore < computerScore) {
-        console.log(
-            `Oh-No You Lost! Player Score:${userScore} - Computer Score: ${computerScore}`
-        );
-    } else {
-        console.log(
-            `It\'s a DRAW! Player Score:${userScore} - Computer Score: ${computerScore}`
-        );
-    }
-}
-
-game();
+//Event listner
+choices.forEach(choice => choice.addEventListener('click', play));
